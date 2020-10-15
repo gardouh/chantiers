6370b60d0fe03918af9fab7ded112edb3e76c85d
gardouh <gardouhkhalil1@gmail.com>
2020-10-15 14:59:22 +0200

    Add chantiers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,84 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet, StatusBar } from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import React, { Component } from 'react';
+import { View, Text, Button,ScrollView, StyleSheet, StatusBar } from 'react-native';
+import Row from './RowTask';
+import { FloatingAction } from "react-native-floating-action";
+import {TouchableOpacity } from 'react-native'
+import { Root, Popup } from 'popup-ui'
 
-const HomeScreen = ({navigation}) => {
-
-  const { colors } = useTheme();
-
-  const theme = useTheme();
-  
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle= { theme.dark ? "light-content" : "dark-content" }/>
-        <Text style={{color: colors.text}}>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => navigation.navigate("Details")}
-      />
-      </View>
-    );
-};
-
-export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+const actions = [
+  {
+    text: "Arrivée",
+    icon: require("../assets/ic_accessibility_white.png"),
+    name: "bt_accessibility",
+    position: 2
   },
-});
+  {
+    text: "Départ",
+    icon: require("../assets/ic_language_white.png"),
+    name: "bt_language",
+    position: 1
+  }
+];
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
+            const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+            },
+            scrollView: {
+                backgroundColor: '#F5F5F5',
+                flex: 1,
+                padding: 10,
+                paddingTop: STATUSBAR_HEIGHT,
+            },
+            });
+
+            
+export default class HomeScreen extends Component {
+        
+
+        constructor(props) {
+            super(props);
+            
+        }
+      
+        render() {
+          return (
+            
+            <View style={styles.container}>
+              <Root>
+                <StatusBar
+                barStyle="light-content"
+                />
+                
+                <ScrollView
+                style={styles.scrollView}
+                >
+                    <Row zIndex={100} />
+                    <Row zIndex={90} />
+                    <Row zIndex={80} />
+                    <Row zIndex={70} />
+                    <Row zIndex={60} />
+                    <Row zIndex={50} />
+                    <Row zIndex={40} />
+                    <Row zIndex={30} />
+                </ScrollView>
+                
+                <FloatingAction
+                    actions={actions}
+                    onPressItem={name => {
+                      Popup.show({
+                        type: 'Success',
+                        title: 'Pointage pris en compte',
+                        button: true,
+                        textBody: 'Vous avez pointé à 11h15',
+                        buttonText: 'Ok',
+                        callback: () => Popup.hide()
+                      })
+                    }}
+                />
+              </Root>
+            </View>
+          );
+        }
+      }
